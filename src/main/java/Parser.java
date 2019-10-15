1c3a3dca75d96d29d1dd0bb430b37b38c0fb365b
Mirzoeva <vselennaya314@gmail.com>
2019-10-15 16:43:18 +0300

    Scripted auto-commit on change (2019-10-15 16:43:18) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -11,7 +11,7 @@ public class Parser {
         //Default seperator is comma
         //Default quote character is double quote
         //Start reading from line number 2 (line numbers start from zero)
-        CSVReader reader = new CSVReader(new FileReader("data.csv"), ',' , '"' , 1);
+        CSVReader reader = new CSVReader(new FileReader("/Users/umamirzoeva/Parallel/Lab2/src/main/resources/L_AIRPORT_ID.csv"), ',' , '"' , 1);
         //Read CSV line by line and use the string array as you want
         String[] nextLine;
         while ((nextLine = reader.readNext()) != null) {

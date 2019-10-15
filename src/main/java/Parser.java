1b30bc752d0c161fcd966a734274e87c2e11df07
Mirzoeva <vselennaya314@gmail.com>
2019-10-15 18:15:57 +0300

    Scripted auto-commit on change (2019-10-15 18:15:57) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -7,11 +7,11 @@ public class Parser {
     @SuppressWarnings("resource")
     public static void main(String[] args) throws Exception {
         //Build reader instance
-        //Read data.csv
-        //Default seperator is comma
-        //Default quote character is double quote
-        //Start reading from line number 2 (line numbers start from zero)
-        CSVReader reader = new CSVReader(new FileReader("/Users/umamirzoeva/Parallel/Lab2/src/main/resources/L_AIRPORT_ID.csv"), ',' , '"' , 1);
+        //        //Read data.csv
+        //        //Default seperator is comma
+        //        //Default quote character is double quote
+        //        //Start reading from line number 2 (line numbers start from zero)
+        CSVReader reader = new CSVReader(new FileReader("/Users/umamirzoeva/Parallel/Lab2/src/main/resources/L_AIRPORT_ID.csv"), ',' , '"' , 0);
         //Read CSV line by line and use the string array as you want
         String[] nextLine;
         while ((nextLine = reader.readNext()) != null) {

9f8ead2cdfe8a5eb6ea03d18b9484bafe85479ee
Mirzoeva <vselennaya314@gmail.com>
2019-10-18 13:53:52 +0300

    Scripted auto-commit on change (2019-10-18 13:53:52) by gitwatch.sh

--- src/main/java/lab2/TextPair.java
+++ src/main/java/lab2/TextPair.java
@@ -1,4 +1,73 @@
 package lab2;
 
-public class TextPair {
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.WritableComparable;
+
+
+public class TextPair implements WritableComparable<TextPair>{
+    private Text first;
+    private Text second;
+
+    public TextPair(){
+        this.first=new Text();
+        this.second=new Text();
+    }
+
+    public TextPair(Text first, Text second) {
+        this.first = first;
+        this.second = second;
+    }
+    public TextPair(String first,String second){
+        this.first=new Text(first);
+        this.second=new Text(second);
+    }
+
+    public Text getFirst() {
+        return first;
+    }
+
+    public Text getSecond() {
+        return second;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.parseInt(first.toString());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof TextPair){
+            TextPair tp=(TextPair)obj;
+            return first.equals(tp.getFirst());
+        }
+        return false;
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        first.readFields(in);
+        second.readFields(in);
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        first.write(out);
+        second.write(out);
+    }
+
+
+    @Override
+    public int compareTo(TextPair tp) {
+        int cmp=first.compareTo(tp.getFirst());
+        if(cmp!=0)
+            return cmp;
+        return second.compareTo(tp.getSecond());
+
+    }
+
 }

0f9fb2cf6996f536bdc19139b571bc90266f90c5
Mirzoeva <vselennaya314@gmail.com>
2019-10-19 02:36:57 +0300

    Scripted auto-commit on change (2019-10-19 02:36:57) by gitwatch.sh

--- src/main/java/lab2/Parser.java
+++ src/main/java/lab2/Parser.java
@@ -9,14 +9,14 @@ public class Parser {
     }
 
     public String getKey(){
-        if (mainString.length > 1 && !mainString[0].isEmpty())
+        if (mainString.length >= 1 && !mainString[0].isEmpty())
             return mainString[0];
         else
             return String.valueOf(0.0);
     }
 
     public String getValue(){
-        if (mainString.length > 2 && !mainString[1].isEmpty())
+        if (mainString.length >= 1 && !mainString[1].isEmpty())
             return mainString[1];
         else
             return String.valueOf(0.0);

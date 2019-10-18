3ced469337bbbd3e9bb1ce9808096ffa5d0a8d52
Mirzoeva <vselennaya314@gmail.com>
2019-10-18 23:21:40 +0300

    Scripted auto-commit on change (2019-10-18 23:21:40) by gitwatch.sh

--- src/main/java/lab2/FirstPartitioner.java
+++ src/main/java/lab2/FirstPartitioner.java
@@ -1,13 +1,10 @@
 package lab2;
-import jdk.internal.org.objectweb.asm.tree.analysis.Value;
-import org.apache.hadoop.mapreduce.Partitioner;
-import java.security.Key;
+import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
+import org.w3c.dom.Text;
 
-public class FirstPartitioner extends Partitioner<Key, Value> {
-    public FirstPartitioner(){}
-    @Override
-    public int getPartition(Key key, Value value, int numReduceTasks){
+
+public class FirstPartitioner extends HashPartitioner{
+    public int getPartition(Text key, Text value, int numReduceTasks){
         return (key.hashCode()&Integer.MAX_VALUE)% numReduceTasks;
     }
-
 }

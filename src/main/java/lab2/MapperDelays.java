0f4c02d8a5ed163a9750673a72ed704584e63335
Mirzoeva <vselennaya314@gmail.com>
2019-10-19 01:07:36 +0300

    Scripted auto-commit on change (2019-10-19 01:07:36) by gitwatch.sh

--- src/main/java/lab2/MapperDelays.java
+++ src/main/java/lab2/MapperDelays.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 public class MapperDelays extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        
         TextPair delay = new TextPair(new Text(new ParserDelays(value.toString()).getKey()), new Text("1"));
-        context.write(delay, new Text(new ParserDelays(value.toString()).getValue()));
+        context.write(delay, new Text();
     }
 }

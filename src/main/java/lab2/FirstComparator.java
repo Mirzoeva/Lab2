10e02f4e922b9ff91ed9555040034438e275f04f
Mirzoeva <vselennaya314@gmail.com>
2019-10-18 21:11:33 +0300

    Scripted auto-commit on change (2019-10-18 21:11:33) by gitwatch.sh

--- src/main/java/lab2/FirstComparator.java
+++ src/main/java/lab2/FirstComparator.java
@@ -3,7 +3,7 @@ package lab2;
 import org.apache.hadoop.io.WritableComparator;
 
 public class FirstComparator extends WritableComparator {
-    
+    public FirstComparator 
     public int compare(TextPair a, TextPair b){
         return a.compareTo(b);
     }

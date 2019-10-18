694e25e36a9df10fb35e02c65047e084c0ebc060
Mirzoeva <vselennaya314@gmail.com>
2019-10-19 02:44:51 +0300

    Scripted auto-commit on change (2019-10-19 02:44:51) by gitwatch.sh

--- src/main/java/lab2/ParserDelays.java
+++ src/main/java/lab2/ParserDelays.java
@@ -12,14 +12,14 @@ public class ParserDelays {
         if (mainString.length > 15 && !mainString[14].isEmpty())
             return mainString[14];
         else
-            return String.valueOf(0.0);
+            return "";
     }
 
     public String getValue(){
         if (mainString.length > 19 && !mainString[18].isEmpty())
             return mainString[18];
         else
-            return String.valueOf(0.0);
+            return "";
     }
 
 }

e0a5b5c80b6a79635e204fd84bbb58d11bb991e2
Mirzoeva <vselennaya314@gmail.com>
2019-10-18 12:34:22 +0300

    Scripted auto-commit on change (2019-10-18 12:34:22) by gitwatch.sh

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -6,11 +6,6 @@ import au.com.bytecode.opencsv.CSVReader;
 public class Parser {
     @SuppressWarnings("resource")
     public static void main(String[] args) throws Exception {
-        //Build reader instance
-        //        //Read data.csv
-        //        //Default seperator is comma
-        //        //Default quote character is double quote
-        //        //Start reading from line number 2 (line numbers start from zero)
         CSVReader reader = new CSVReader(new FileReader("/Users/umamirzoeva/Parallel/Lab2/src/main/resources/L_AIRPORT_ID.csv"), ',' , '"' , 0);
         //Read CSV line by line and use the string array as you want
         String[] nextLine;

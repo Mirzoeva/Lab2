7204e92caf39b0032b0805182ea93cc37665bb18
Mirzoeva <vselennaya314@gmail.com>
2019-10-19 01:14:37 +0300

    Scripted auto-commit on change (2019-10-19 01:14:37) by gitwatch.sh

--- src/main/java/lab2/FirstPartitioner.java
+++ src/main/java/lab2/FirstPartitioner.java
@@ -3,7 +3,7 @@ import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
 import org.w3c.dom.Text;
 
 
-public class FirstPartitioner extends HashPartitioner<FirstComparator, >{
+public class FirstPartitioner extends HashPartitioner<FirstComparator, Text>{
     public int getPartition(Text key, Text value, int numReduceTasks){
         return (key.hashCode()&Integer.MAX_VALUE)% numReduceTasks;
     }

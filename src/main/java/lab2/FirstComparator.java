ec14c7176046f0d2ec7f7012d586cc0e147fddbc
Mirzoeva <vselennaya314@gmail.com>
2019-10-18 21:11:05 +0300

    Scripted auto-commit on change (2019-10-18 21:11:05) by gitwatch.sh

--- src/main/java/lab2/FirstComparator.java
+++ src/main/java/lab2/FirstComparator.java
@@ -1,13 +1,10 @@
 package lab2;
 
-import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class FirstComparator extends WritableComparator {
     @Override
     public int compare(TextPair a, TextPair b){
-        //
-        return 
-
+        return a.compareTo(b);
     }
 }

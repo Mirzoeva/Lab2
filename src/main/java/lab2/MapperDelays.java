285a83117c9dfa5bbecb51d981e2814e8c415c7b
Mirzoeva <vselennaya314@gmail.com>
2019-10-18 19:29:11 +0300

    Scripted auto-commit on change (2019-10-18 19:29:11) by gitwatch.sh

--- src/main/java/lab2/MapperDelays.java
+++ src/main/java/lab2/MapperDelays.java
@@ -10,7 +10,6 @@ public class MapperDelays extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         ServiceCall call = new ServiceCall(value);
-        context.write(new TextPair(call.getSystemA().toString(),"1"),
-                new Text(call.toString()));
+        context.write(new TextPair(call.getSystemA().toString(), 1), new Text(call.toString()));
     }
 }

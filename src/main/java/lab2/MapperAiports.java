9c2c887264ddb3bf3636527c88e2aaa74f6a393b
Mirzoeva <vselennaya314@gmail.com>
2019-10-18 14:35:28 +0300

    Scripted auto-commit on change (2019-10-18 14:35:28) by gitwatch.sh

--- src/main/java/lab2/MapperAiports.java
+++ src/main/java/lab2/MapperAiports.java
@@ -1,13 +1,20 @@
 package lab2;
 
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import au.com.bytecode.opencsv.CSVReader;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.io.LongWritable;
-import java.io.IOException;
 
+public class SystemsJoinMapper extends Mapper<LongWritable, Text, TextPair, Text> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        SystemInfo system = new SystemInfo(value);
+        context.write(new TextPair(system.getSystemCode().toString(),"0"), new Text(system.toString()));
+    }
+}
 
 public class MapperAiports extends Mapper<LongWritable, Text, TextPair, Text> {
     @Override

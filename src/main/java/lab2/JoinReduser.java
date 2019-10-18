76a11bf288dd27201ca19efa2c8255214b7ca25c
Mirzoeva <vselennaya314@gmail.com>
2019-10-18 20:24:04 +0300

    Scripted auto-commit on change (2019-10-18 20:24:04) by gitwatch.sh

--- src/main/java/lab2/JoinReduser.java
+++ src/main/java/lab2/JoinReduser.java
@@ -1,4 +1,20 @@
 package lab2;
 
-public class JoinReduser {
+import org.apache.hadoop.mapreduce.Reducer;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+public class JoinReduser extends Reducer<TextPair, Text, Text, Text> {
+    @Override
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()){
+            Text call = iter.next();
+            Text outValue = new Text(call.toString() + "\t" + systemInfo.toString());
+            context.write(key.getFirst(), outValue);
+        }
+    }
 }

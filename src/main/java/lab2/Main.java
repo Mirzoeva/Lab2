5106ffb76dc8ed19d297c99a6cdd2229a78c1aa3
Mirzoeva <vselennaya314@gmail.com>
2019-10-18 14:10:43 +0300

    Scripted auto-commit on change (2019-10-18 14:10:43) by gitwatch.sh

--- src/main/java/lab2/Main.java
+++ src/main/java/lab2/Main.java
@@ -19,7 +19,7 @@ public class Main {
         job.setJarByClass(Main.class);
         job.setJobName("JoinJob sort");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, MapperAiports.class);
-        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, MapperDelays.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job.setPartitionerClass(TextPair.FirstPartitioner.class);
         job.setGroupingComparatorClass(TextPair.FirstComparator.class);

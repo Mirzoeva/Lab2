bfb3ce987a602230e8916df8c9213689f52fdf9a
Mirzoeva <vselennaya314@gmail.com>
2019-10-19 13:05:34 +0300

    Scripted auto-commit on change (2019-10-19 13:05:34) by gitwatch.sh

--- src/main/java/lab2/Parser.java
+++ src/main/java/lab2/Parser.java
@@ -4,11 +4,15 @@ public class Parser {
     private String[] mainString;
 
     public Parser(String str){
+        System.out.println("-------------------------------");
         String[] string = str.split(",");
         for (int i = 0; i < string.length; i++){
             string[i] = string[i].replaceAll("\"", "");
+            System.out.println("SUKA" + string[i] + "\n");
         }
         mainString = string;
+        System.out.println("__________________________________________");
+
     }
 
     public String getKey(){

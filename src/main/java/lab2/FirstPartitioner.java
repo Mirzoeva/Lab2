b17bc8ef5712fcaa8cbe84d556cca67ac061f2e1
Mirzoeva <vselennaya314@gmail.com>
2019-10-22 20:16:17 +0300

    Scripted auto-commit on change (2019-10-22 20:16:17) by gitwatch.sh

--- src/main/java/lab2/FirstPartitioner.java
+++ src/main/java/lab2/FirstPartitioner.java
@@ -6,6 +6,6 @@ import org.apache.hadoop.io.Text;
 public class FirstPartitioner extends Partitioner<TextPair, Text>{
     @Override
     public int getPartition(TextPair key, Text value, int numReduceTasks){
-        return key.getFirst().hashCode()% numReduceTasks;
+        return key.getFirst().hashCode() % numReduceTasks;
     }
 }

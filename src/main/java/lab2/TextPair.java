7b2c7b8b003d378dce345b4d83de9f3dc505be20
Mirzoeva <vselennaya314@gmail.com>
2019-10-22 20:20:03 +0300

    Scripted auto-commit on change (2019-10-22 20:20:02) by gitwatch.sh

--- src/main/java/lab2/TextPair.java
+++ src/main/java/lab2/TextPair.java
@@ -35,14 +35,14 @@ public class TextPair implements WritableComparable<TextPair>{
         return second;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if(obj instanceof TextPair){
-            TextPair tp=(TextPair)obj;
-            return first.equals(tp.getFirst()) && second.equals(tp.getSecond());
-        }
-        return false;
-    }
+//    @Override
+//    public boolean equals(Object obj) {
+//        if(obj instanceof TextPair){
+//            TextPair tp=(TextPair)obj;
+//            return first.equals(tp.getFirst()) && second.equals(tp.getSecond());
+//        }
+//        return false;
+//    }
 
     @Override
     public void readFields(DataInput in) throws IOException {
@@ -62,7 +62,7 @@ public class TextPair implements WritableComparable<TextPair>{
         if (f == 0){
             return second.compareTo(o.getSecond());
         }
-        return 0;
+        return f;
     }
 
     @Override

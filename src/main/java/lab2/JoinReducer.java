a667761fc53aa69ea2d6aa4e7c772759c1194a1d
Mirzoeva <vselennaya314@gmail.com>
2019-10-22 15:06:58 +0300

    Scripted auto-commit on change (2019-10-22 15:06:58) by gitwatch.sh

--- src/main/java/lab2/JoinReducer.java
+++ src/main/java/lab2/JoinReducer.java
@@ -2,12 +2,13 @@ package lab2;
 
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.io.Text;
+
 import java.io.IOException;
 import java.util.Iterator;
 
 public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
     @Override
-    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         StringBuilder s = new StringBuilder("FUCK THIS\n");
 
@@ -16,26 +17,24 @@ public class JoinReducer extends Reducer<TextPair, Text, Text, Text> {
         float minTime = Integer.MAX_VALUE;
         float sum = 0;
         Text airport = new Text(iter.next().toString() + ",");
-        s= s.append(airport);
-            while (iter.hasNext()){
-                String call = iter.next().toString();
-                try {
-
-                    float correntTime;
+        s = s.append(airport);
+        while (iter.hasNext()) {
+            String call = iter.next().toString();
+            try {
+                float correntTime;
                 correntTime = Float.parseFloat(call);
                 if (correntTime > maxTime)
                     maxTime = correntTime;
                 if (correntTime < minTime)
                     minTime = correntTime;
                 sum += correntTime;
-                }catch (NumberFormatException e){
-                }
-                count++;
-            }
+            } catch (NumberFormatException e) {}
+            count++;
+        }
 
 
-        if (count != 0){
-            float average = sum/count;
+        if (count != 0) {
+            float average = sum / count;
             context.write(airport, new Text("Min: " + minTime + ", Max: " + maxTime + ", Average: " + average));
         }
     }

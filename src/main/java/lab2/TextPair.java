6f27c93218a34dfeea0d52a2a62ef824e770824b
Mirzoeva <vselennaya314@gmail.com>
2019-10-22 15:12:38 +0300

    Scripted auto-commit on change (2019-10-22 15:12:38) by gitwatch.sh

--- src/main/java/lab2/TextPair.java
+++ src/main/java/lab2/TextPair.java
@@ -68,4 +68,8 @@ public class TextPair implements WritableComparable<TextPair>{
 
     }
 
+    @Override
+    public String toString() {
+        return first.toString()+"_"+second.toString();
+    }
 }

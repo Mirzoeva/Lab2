6f6c21b40458634714be7c421f5dda4d1e5d026f
Mirzoeva <vselennaya314@gmail.com>
2019-10-22 14:49:13 +0300

    Scripted auto-commit on change (2019-10-22 14:49:13) by gitwatch.sh

--- src/main/java/lab2/ParserDelays.java
+++ src/main/java/lab2/ParserDelays.java
@@ -8,9 +8,7 @@ public class ParserDelays {
 //        for (int i = 0; i < string.length; i++){
 //            System.out.println("SUKA" + string[i] + "\n");
 //        }
-        for(int i = 0; i < string.length; i++){
-            string[i] = string[i].stripLeading("s")
-        }
+       
         mainString = string;
     }
 

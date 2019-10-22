5502bb77387da82c6b09f89a12931d91ddf5fb29
Mirzoeva <vselennaya314@gmail.com>
2019-10-22 14:46:37 +0300

    Scripted auto-commit on change (2019-10-22 14:46:37) by gitwatch.sh

--- src/main/java/lab2/ParserDelays.java
+++ src/main/java/lab2/ParserDelays.java
@@ -4,10 +4,13 @@ public class ParserDelays {
     private String[] mainString;
 
     public ParserDelays(String str){
-        String[] string = str.split(",[\"]");
+        String[] string = str.split(",");
 //        for (int i = 0; i < string.length; i++){
 //            System.out.println("SUKA" + string[i] + "\n");
 //        }
+        for(int i = 0; i < string.length; i++){
+            string[i] = String.
+        }
         mainString = string;
     }
 

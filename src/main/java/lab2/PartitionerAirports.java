bcfdae4030321124c0ee8c51469a37cb0d247765
Mirzoeva <vselennaya314@gmail.com>
2019-10-24 22:01:01 +0300

    Scripted auto-commit on change (2019-10-24 22:01:01) by gitwatch.sh

--- src/main/java/lab2/PartitionerAirports.java
+++ src/main/java/lab2/PartitionerAirports.java
@@ -6,7 +6,7 @@ import org.apache.hadoop.io.Text;
 public class PartitionerAirports extends Partitioner<TextPair, Text>{
     @Override
     public int getPartition(TextPair key, Text value, int numReduceTasks){
-        return key.getKey().hashCode() % numReduceTasks;
+        return (key.getKey().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
     }
 }
 
